81396466ec237c82ab523f14976f7eaf566ef840
Leonova-Tatiana <tanysha155@icloud.com>
2025-03-16 10:05:34 +0200

    task

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,15 @@
-function getShippingMessage(country, price, deliveryFee) {
-    // Обчислення загальної вартості замовлення
-    const totalPrice = price + deliveryFee;
-    
-    // Повертаємо рядок із повідомленням
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
+    return message;
+  }
+
+  return message.slice(0, maxLength) + "...";
 }
 
-// Перевірка коректності роботи функції
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+// Перевірка функції
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,13 @@
-function getElementWidth(content, padding, border) {
-    // Перетворення значень у числа, видаляючи 'px'
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2; // Для обох сторін
-    const borderWidth = parseFloat(border) * 2;   // Для обох сторін
-    
-    // Обчислення загальної ширини елемента
-    return contentWidth + paddingWidth + borderWidth;
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
 
-// Перевірка коректності роботи функції
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// Перевірка функції
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
